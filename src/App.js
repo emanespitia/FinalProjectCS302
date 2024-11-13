74ea9b4b32b227360537073c0c6e811abaff0297
emanespitia <eespiti@jacksonville.edu>
2024-11-13 14:54:24 -0500

    undo

--- src/App.js
+++ src/App.js
@@ -24,19 +24,3 @@ function App() {
 
 export default App;
 
-function VideoList({ videos, emptyHeading }) {
-  const count = videos.length;
-  let heading = emptyHeading;
-  if (count > 0) {
-    const noun = count > 1 ? 'Videos' : 'Video';
-    heading = count + ' ' + noun;
-  }
-  return (
-    <section>
-      <h2>{heading}</h2>
-      {videos.map(video =>
-        <Video key={video.id} video={video} />
-      )}
-    </section>
-  );
-}
